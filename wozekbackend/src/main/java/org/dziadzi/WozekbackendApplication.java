ec4148dbafa435778b53e725d3eda9c2177c7bac
Karol <karol.kuc@apollogic.com>
2016-03-17 16:42:05 +0100

    knowledge representation backend

---
+++ wozekbackend/src/main/java/org/dziadzi/WozekbackendApplication.java
@@ -0,0 +1,12 @@
+package org.dziadzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WozekbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WozekbackendApplication.class, args);
+	}
+}
